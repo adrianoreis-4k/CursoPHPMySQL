4d9ec6b3ecaf5ae6ce49cb55629b4663d577ef0d
Adriano Reis <adrianoreis134@hotmail.com>
2024-03-29 18:58:06 -0400

    Configurando o script de seleção de páginas usando Switch

--- PHP/aula/index.php
+++ PHP/aula/index.php
@@ -14,7 +14,17 @@ else{
     $pagina = 'home';
 }
 
+switch($pagina){
+    case 'cursos': include 'views/cursos.php'; break;
+    case 'alunos': include 'views/alunos.php'; break;
+    case 'matriculas': include 'views/matriculas.php'; break;
+    case 'inserir_curso': include 'views/inserir_curso.php'; break;
+    case 'inserir_aluno': include 'views/inserir_aluno.php'; break;
+    case 'inserir_matricula': include 'views/inserir_matricula.php'; break;
+    default: include 'views/home.php'; break;
+}
 
+/*
 if($pagina == 'cursos'){
     include 'views/cursos.php';
 }
@@ -36,7 +46,8 @@ elseif($pagina == 'matriculas'){
 else{
     include 'views/home.php';
 }
+*/
+
 
 # Rodapé
 include 'footer.php';
-
