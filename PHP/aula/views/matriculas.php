609465b05b55ce2c30b24149eb0c43dde317a5f8
Adriano Reis <adrianoreis134@hotmail.com>
2024-03-29 22:25:13 -0400

    Criando um sistema de deleção de dados do MySQL

--- PHP/aula/views/matriculas.php
+++ PHP/aula/views/matriculas.php
@@ -9,7 +9,11 @@
     <?php
         while($linha = mysqli_fetch_array($consulta_matriculas)){
             echo '<tr><td>'.$linha['nome_aluno'].'</td>';
-            echo '<td>'.$linha['nome_curso'].'</td></tr>';
+            echo '<td>'.$linha['nome_curso'].'</td>';
+    ?>
+            <td><a href="deleta_matricula.php?id_aluno_curso=<?php echo $linha['id_aluno_curso']; ?>">Deletar</a></td></tr>
+        
+    <?php
         }
     ?>
 </table>
